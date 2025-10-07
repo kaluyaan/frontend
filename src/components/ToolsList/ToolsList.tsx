3319f679d58d05e3151c1c2ad57a58cc50c64e4b
SandeepSokle <sandeepsokle12@gmail.com>
2025-10-07 22:08:47 +0530

    added loader when click on tool

--- src/components/ToolsList/ToolsList.tsx
+++ src/components/ToolsList/ToolsList.tsx
@@ -1,13 +1,21 @@
 "use client";
-import React from "react";
+import React, { useState } from "react";
 import Link from "next/link";
 import { toolsList } from "@/config/constants/homeConstants";
 import styles from "../Home/home.module.css";
 import { useSearchParams } from "next/navigation";
+import ToolClickLoader from "../Loading/ToolClickLoader";
 
 function ToolsList() {
   const searchParams = useSearchParams();
   const category = searchParams.get("category")?.toLocaleLowerCase();
+  const [isLoading, setIsLoading] = useState(false);
+  const [toolName, setToolName] = useState("")
+
+   const handleClick = (name: string) => {
+    setIsLoading(true);
+    setToolName(name);
+  };
 
   return (
     <section className={styles.toolsGrid}>
@@ -34,12 +42,14 @@ function ToolsList() {
               e.currentTarget.style.transform = "translateY(0)";
               e.currentTarget.style.boxShadow = "0 5px 20px rgba(0, 0, 0, 0.1)";
             }}
+            onClick={() => handleClick(tool.title)}
           >
             <div className={styles.toolIcon}>{tool.icon}</div>
             <h3 className={styles.toolTitle}>{tool.title}</h3>
             <p className={styles.toolDesc}>{tool.desc}</p>
           </Link>
         ))}
+        <ToolClickLoader isLoading={isLoading} toolName={toolName} />
     </section>
   );
 }
